d43e385eaf47dfabed5dfea85ab7be82a9e76d75
jrangelardila <jrangelardila@gmail.com>
2025-10-21 21:21:51 -0500

    Fixed: Missing Header and Copyright Information in Files #2

--- amd/src/div2.js
+++ amd/src/div2.js
@@ -1,3 +1,27 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Handles the dashboard interactions for tutor follow-up data.
+ *
+ * @module     tool_tutor_follow/div2
+ * @package    tool_tutor_follow
+ * @copyright  2025 Jhon Rangel Ardila
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 define([
     "jquery",
     'core/ajax',
